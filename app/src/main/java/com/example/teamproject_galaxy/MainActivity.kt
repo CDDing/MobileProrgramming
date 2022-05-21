d1b2d9faf4b18efcc398aef6b83c49f64a996929
Myeong Geun <jangmee95@naver.com>
2022-05-21 22:05:31 +0900

    Test

--- app/src/main/java/com/example/teamproject_galaxy/MainActivity.kt
+++ app/src/main/java/com/example/teamproject_galaxy/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.teamproject_galaxy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("Test","테스트입니다")
     }
 }
