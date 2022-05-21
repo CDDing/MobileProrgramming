4e0d6e2c291428026093b21e749d04048b546989
Myeong Geun <jangmee95@naver.com>
2022-05-21 22:09:17 +0900

    확인

--- app/src/main/java/com/example/teamproject_galaxy/MainActivity.kt
+++ app/src/main/java/com/example/teamproject_galaxy/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("Test","테스트입니다")
+        Log.i("확인","테스트입니다")
     }
 }
