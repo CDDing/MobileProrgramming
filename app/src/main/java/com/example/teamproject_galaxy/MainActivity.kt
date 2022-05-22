916d313dcc3fa08ceffd2b3602322b167b64da45
Myeong Geun <jangmee95@naver.com>
2022-05-22 20:50:21 +0900

    Main Activity 수정

--- app/src/main/java/com/example/teamproject_galaxy/MainActivity.kt
+++ app/src/main/java/com/example/teamproject_galaxy/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.teamproject_galaxy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,4 +11,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Log.i("확인합니다","테스트입니다")
     }
+}
+fun main(){
+    val api_key:String="74795954496a616e35354745524177"
+    val RequestSubwayData:String="http://swopenapi.seoul.go.kr/api/subway/"+api_key+"/json/realtimePosition/0/5/1호선"
+    val URL=URL(RequestSubwayData)
+    val conn=URL.openConnection()
+
+    print(conn)
 }
